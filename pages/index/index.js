ed9e6113722e1690c7a6f69e08c0cb62cb401173
Kenny <liangwc27@163.com>
2018-05-18 17:42:59 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,95 @@
+// pages/index/index.js
+let app = getApp();
+console.log('app',app);
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg: `This is message`,
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+  changeTextColor() {
+    this.setData({
+      msg: '6666'
+    })
+  },
+  viewToMY() {
+    wx.switchTab({
+      url: "/pages/logs/logs?id=1"
+    })
+  },
+  toViewDetail() {
+    wx.redirectTo({
+      url: '/pages/detail/detail',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url: 'http://xiaochengxu.test.com:81',
+      method: "POST"
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
